21c8cbf79e4c95a102d8ce317cab7f726118888a
CaioAugustoHD <caioaugustosbs@gmail.com>
2022-10-19 20:07:00 -0300

    Criado array com os dados dos carros

--- tabela-carros/src/App.jsx
+++ tabela-carros/src/App.jsx
@@ -1,5 +1,14 @@
 import React from "react"
 
+const carros = [
+  {modelo:"Mobi", marca:"Fiat", preço:"65 mil"},
+  {modelo:"Strada", marca:"Fiat", preço:"127 mil"},
+  {modelo:"Saveiro", marca:"Volkswagen", preço:"90 mil"},
+  {modelo:"Voyage", marca:"Volkswagen", preço:"85 mil"},
+  {modelo:"Hilux", marca:"Toyota", preço:"270 mil"},
+  {modelo:"Corolla", marca:"Toyota", preço:"160 mil"}
+]
+
 export default function App() {
   
   return <h1>Hello word!</h1>
